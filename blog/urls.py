dbfe4b77128e493956f5df23882124508f4032fa
James Pinnock <james-pinnock@hotmail.com>
2019-03-08 18:14:15 +0100

    First commit

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from django.contrib import admin
+from . import views
+
+
+urlpatterns = [
+    url('', views.home, name='blog-home'),
+    url('about/', views.about, name='blog-about'),
+]
+
+
