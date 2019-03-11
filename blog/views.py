8a1dd2d7c80c530f3de74c0191ddf085c0b91af2
James Pinnock <james-pinnock@hotmail.com>
2019-03-11 19:11:34 +0100

    Passes the list to be accessed in the home page

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
 
+    {
+        "text":"text value 1",
+        "textA":"text value 2",
+        "textB":"text value 3"
+    },
+    {
+
+        "text":"text value 01",
+        "textD":"text value 02 ",
+        "textF":"text value 03"
+    }
+]
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts':posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html')
