9f44f0c358bd4b20c383eabe0610a45d83a4d788
James Pinnock <james-pinnock@hotmail.com>
2019-03-26 17:44:45 +0100

    Added the vehicle class to db

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Vehicle(models.Model):
+    name = models.CharField(max_length=100)
+    platenumber = models.CharField(max_length=9)
+    date_added = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
