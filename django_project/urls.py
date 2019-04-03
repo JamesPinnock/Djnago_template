147fb3be1b7d483eba46c3b4650048fd89fbb5c3
James Pinnock <james-pinnock@hotmail.com>
2019-04-03 18:12:34 +0100

    Users register form added, not polished thus far

--- django_project/urls.py
+++ django_project/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from users import views as user_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url('', include('blog.urls')),
+    url("register/", user_views.register, name='register')
 ]
 
