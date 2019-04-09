42f31721afbacdbb097ae71aec8936ddde4a7b16
James Pinnock <james-pinnock@hotmail.com>
2019-04-09 18:12:43 +0100

    Added the links to the login and register page, Users credentials work and redirect to home page

--- django_project/urls.py
+++ django_project/urls.py
@@ -16,10 +16,14 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from users import views as user_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url('', include('blog.urls')),
-    url("register/", user_views.register, name='register')
+    url("register/", user_views.register, name='register'),
+    url("login/", auth_views.LoginView.as_view(template_name="users/login.html"), name='login'),
+    url("logout/", auth_views.LogoutView.as_view(template_name="users/logout.html"), name='logout')
+
 ]
 
--- users/views.py
+++ users/views.py
@@ -1,6 +1,5 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from .forms import UserRegisterForm
 
 def register(request):
